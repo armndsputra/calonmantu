7df5c1e582910447a14792149d15f57779b977de
unknown <trisna.tkjtamsisjetis14@gmail.com>
2026-03-05 09:21:08 +0700

    api login

--- src/pages/admin/articles.jsx
+++ src/pages/admin/articles.jsx
@@ -4,7 +4,31 @@ export default function Articles () {
 
     return(
         <div className={styles.article}>
-            dsf
+            <h3>Article</h3>
+            <div className={styles.tableGroup}>
+                <table>
+                    <thead>
+                        <tr>
+                        <th>no</th>
+                        <th>title</th>
+                        <th>ID</th>
+                        <th>author</th>
+                        <th>action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>gurun pasir</td>
+                        <td>sfdsfsdds</td>
+                        <td>trisna</td>
+                        <td>
+
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 
--- src/pages/main/login.jsx
+++ src/pages/main/login.jsx
@@ -1,6 +1,38 @@
 import styles from './login.module.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function Login() {
+    const [falseData, setFalseData] = useState('')
+    const [form, setForm] = useState({ email: '', password: '' })
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+
+        try {
+            const isEmail = form.email
+            const isPassword = form.password
+
+            if (isEmail && isPassword) {
+                axios
+                    .post('https://calonmantu.sbs/api/account/login', {
+                        email: isEmail,
+                        password: isPassword,
+                    })
+                    .then(response => {
+                        console.log(response)
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        setFalseData('Kata Sandi atau Email Salah!')
+                    })
+            } else {
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
         <div className={styles.login}>
             <div className={styles.card}>
@@ -8,19 +40,32 @@ export default function Login() {
                     <h1>Login</h1>
                 </div>
                 <div className={styles.form}>
-                    <form action="">
+                    <form action="" onSubmit={handleSubmit}>
                         <div className={styles.formGroup}>
                             <label htmlFor="email">Email</label>
-                            <input type="email" id="email" name="email" />
+                            <input
+                                value={form.email}
+                                onChange={e => setForm({ ...form, email: e.target.value })}
+                                type="email"
+                                id="email"
+                                name="email"
+                            />
                         </div>
                         <div className={styles.formGroup}>
                             <label htmlFor="password">Kata Sandi</label>
-                            <input type="password" id="password" name="password" />
+                            <input
+                                value={form.password}
+                                onChange={e => setForm({ ...form, password: e.target.value })}
+                                type="password"
+                                id="password"
+                                name="password"
+                            />
                         </div>
                         <button type="submit">Masuk</button>
                     </form>
                 </div>
                 <div className={styles.footer}>
+                    {falseData && <p className={styles.formInfo}>{falseData}</p>}
                     <p>
                         Apakah kamu belum memiliki akun? <a href="/daftar">Daftar</a>
                     </p>
