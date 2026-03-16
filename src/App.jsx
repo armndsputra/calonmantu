287a26f338f3132d0e459e126a19cd6e28813d9c
unknown <trisna.tkjtamsisjetis14@gmail.com>
2026-03-16 21:12:44 +0700

    update home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // npm package manager
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
-import 'quill/dist/quill.snow.css'; // Import global CSS Quill
+// Import global CSS Quill
+import 'quill/dist/quill.snow.css'; 
 
 // To Routes Folder
 import MainRouter from './routes/MainRouter'
--- src/pages/sites/home.jsx
+++ src/pages/sites/home.jsx
@@ -1,21 +1,25 @@
-import { FaRegComment } from 'react-icons/fa6'
+// npm package manager
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import axios from 'axios'
 import { useLocation } from 'react-router-dom'
 
+// icons
+import { FaRegComment } from 'react-icons/fa6'
 import Loading from '../../assets/Loading.gif'
 
 // css
 import styles from './Home.module.css'
 
+// services
+import Api from '../../services/api/Api'
+
 export default function Home() {
     const location = useLocation()
     const { search } = location.state || {}
 
-    const [contents, setContents] = useState([])
+    const [data, setData] = useState([]) // Data Artikel
     const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
+    const [error, setError] = useState(null) // Data Error Message
 
     // pagination
     const [startPage, setStartPage] = useState(0)
@@ -33,30 +37,23 @@ export default function Home() {
 
     useEffect(() => {
         if (search) {
-            axios
-                .post('https://calonmantu.sbs/api/post/keywords?limit=0&offset=0', {
-                    keywords: search,
-                })
+            Api.post('/api/post/keywords?limit=0&offset=0', {
+                keywords: search,
+            })
                 .then(response => {
                     // console.log(response.data.data)
-                    setContents(response.data.data)
+                    setData(response.data.data)
                     setLoading(false)
                 })
                 .catch(err => {
                     console.log(err)
                 })
         } else {
-            // Jika buka halaman home biasa
-            axios
-                .get(
-                    'https://calonmantu.sbs/api/post/?limit=' +
-                        limitPerPage +
-                        '&offset=' +
-                        startPage
-                )
+            // if normal home
+            Api.get('/api/post/?limit=' + limitPerPage + '&offset=' + startPage)
                 .then(response => {
                     // console.log(response.data.data)
-                    setContents(response.data.data)
+                    setData(response.data.data)
                     setTotalData(response.data.total)
                     setLoading(false)
                 })
@@ -70,44 +67,46 @@ export default function Home() {
     if (loading)
         return (
             <div className="loading">
-                <img src={Loading} alt="Loading..." />
+                <img src={Loading} alt="" />
             </div>
         )
-    if (error) return <div>Error: {error}</div>
+
+    if (error) {
+        console.error(error)
+    }
 
     return (
         <div className={styles.home}>
+
             <div className={styles.pageTitle}>
                 <h3>artikel</h3>
             </div>
-            <div className={styles.container}>
-                {contents.map(content => (
-                    <div key={content.id} className={styles.box}>
-                        {/* <img src={`http://localhost:3000/${content.thumbnail.replace(/^\/?public\/?/i, '')}`} alt="News Image" /> */}
-                        <Link to={`/${content.id}`} className="text-decoration-none">
+            <div className={styles.wrapperHome}>
+                {data.map(e => (
+                    <div key={e.id} className={styles.card}>
+                        <Link to={`/${e.id}`} className="text-decoration-none">
                             <img
-                                src={`https://calonmantu.sbs/${content.thumbnail.replace(/^\/?public\/?/i, '')}`}
-                                alt="News Image"
+                                src={`https://calonmantu.sbs/${e.thumbnail.replace(/^\/?public\/?/i, '')}`}
+                                alt=""
                             />
                         </Link>
-                        {/* <span>{`http://localhost:3000/posts/284917364.jpeg)}`}</span> */}
+
                         <h3 className="">
-                            <Link className={styles.detailLink} to={`/${content.id}`}>
-                                {content.title}
+                            <Link className={styles.targetLink} to={`/${e.id}`}>
+                                {e.title}
                             </Link>
                         </h3>
-                        <span className={styles.writer}>
-                            by{' '}
-                            <span>{content.author.name}</span>
+                        <span className={styles.author}>
+                            by <span>{e.author.name}</span>
                         </span>
-                        <span className={styles.commentCount}>
-                            <FaRegComment className={styles.commentIcon} /> 0
+                        <span className={styles.totalComment}>
+                            <FaRegComment className={styles.iconComment} /> 0
                         </span>
                     </div>
                 ))}
             </div>
-            {/* pagination */}
 
+            {/* pagination */}
             {!search && (
                 <div className={styles.pagination}>
                     <ul>{listItems}</ul>
